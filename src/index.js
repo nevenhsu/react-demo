5a46f7557ac9b604f86dd13605e6a78cb1114717
nevenhsu <nevenhsu@memopresso.com>
2018-07-12 19:25:07 +0800

    connect router redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
@@ -11,11 +11,30 @@ import PostsIndex from './containers/posts_index';
 import PostsNew from './components/post_new';
 import PostsShow from './components/posts_show';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+import { ConnectedRouter } from 'connected-react-router'
+
+import { createBrowserHistory } from 'history'
+
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+
+const history = createBrowserHistory()
+const initialState = {}
+
+const store = createStore(
+  connectRouter(history)(reducers), // new root reducer with router state
+  initialState,
+  compose(
+    applyMiddleware(
+      routerMiddleware(history), // for dispatching history actions
+      promise
+    ),
+  ),
+)
+
 
 ReactDom.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <BrowserRouter>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
             <div>
                 <Switch>
                     <Route path="/posts/new" component={PostsNew}/>
@@ -23,7 +42,7 @@ ReactDom.render(
                     <Route path="/" component={PostsIndex}/>
                 </Switch>
             </div>
-        </BrowserRouter>
+        </ConnectedRouter>
     </Provider>
     , document.querySelector('.container'));
 
